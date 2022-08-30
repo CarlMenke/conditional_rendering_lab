c490c4dcbe995cc289d64ae3fe8dbe2945efc3d7
carlmenke <carlmenke13@gmail.com>
2022-08-30 13:32:16 -0700

    added landing page and input form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import LandingPage from './LandingPage'
+import Info from './Info'
 
 export default function Form(props) {
-  const { currentPage, incrementPage, name, age, email, handleChange } = props
-  return <div></div>
+  const { currentPage, name, age, email, incrementPage, handleChange } = props
+
+
+if(currentPage === 0){
+  return (<LandingPage incrementPage = {incrementPage}/>)
+}
+if(currentPage === 1){
+  return (<Info props = {props}/>
+
+  )
+}
+  
+  return (
+    <div>
+    </div>
+  )
 }
